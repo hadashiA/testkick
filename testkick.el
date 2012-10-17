8cfa58bd9f694db55f612562adc1361715618c80
hadashi <dev@hadashikick.jp>
2012-10-17 22:20:14 +0900

    ちょっとリファクタ

--- testkick.el
+++ testkick.el
@@ -215,11 +215,12 @@
       )))
 
 (defun testkick-test-run (test target)
-  (setq target (case target
-                 (:test-file (testkick-test-test-file test))
-                 (:test-root-directory (testkick-test-test-root-directory test))
-                 (t target)))
-  (compile (concat (testkick-test-command test) " " target)))
+  (compile (concat (testkick-test-command test) " "
+                   (case target
+                     (:test-file (testkick-test-test-file test))
+                     (:test-root-directory (testkick-test-test-root-directory test))
+                     (t target)))
+           ))
 
 ;;
 ;; temp buffer
